2704dd06e1381fcad0cd5690a90cfd6f60f8de26
Aditia Darmadi Jiwandono <aditiadarmadij@gmail.com>
2018-01-12 07:30:08 +0700

    Update level 0 manipulation string

---
+++ index.js
@@ -0,0 +1,13 @@
+/*Level 0*/
+
+const quoteA = `To win big, you sometimes have to take big risks.`;
+const quoteB = `- Bill Gates -`;
+
+console.log(quoteA);
+console.log(quoteB);
+
+/*Level 1*/
+
+/*Level 2*/
+
+/*Level 3*/
