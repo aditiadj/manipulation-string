59c3b73263f4afc7fbf8ba1498726984c2863626
Aditia Darmadi Jiwandono <aditiadarmadij@gmail.com>
2018-01-13 18:41:53 +0700

    delete ';' from index.js

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
 /*Level 0*/
-console.log('---------LEVEL 0------------');
+console.log('---------LEVEL 0------------')
 
-const quoteA = `To win big, you sometimes have to take big risks.`;
-const quoteB = `- Bill Gates -`;
+const quoteA = `To win big, you sometimes have to take big risks.`
+const quoteB = `- Bill Gates -`
 
-console.log(quoteA);
-console.log(quoteB);
+console.log(quoteA)
+console.log(quoteB)
 
 /*Level 1*/
-console.log('---------LEVEL 1------------');
+console.log('---------LEVEL 1------------')
 
 const A = "Jangan"
 const B = "lupa"
@@ -17,22 +17,22 @@ const D = `${A} ${B} ${C}`
 
 console.log(D);
 /*Level 2*/
-console.log('---------LEVEL 2------------');
+console.log('---------LEVEL 2------------')
 
-D.toUpperCase(); //complete sentence to upper
-console.log(D.toUpperCase());
-D.toLowerCase();
-console.log(D.toLowerCase());
+D.toUpperCase() //complete sentence to upper
+console.log(D.toUpperCase())
+D.toLowerCase()
+console.log(D.toLowerCase())
 
 /*Level 3*/
-console.log('---------LEVEL 3------------');
+console.log('---------LEVEL 3------------')
 
 function titleCase(str) {
-  str = str.toLowerCase().split(' ');
+  str = str.toLowerCase().split(' ')
   for (var i = 0; i < str.length; i++) {
-    str[i] = str[i].charAt(0).toUpperCase() + str[i].slice(1);
+    str[i] = str[i].charAt(0).toUpperCase() + str[i].slice(1)
   }
-  return str.join(' ');
+  return str.join(' ')
 }
 
-console.log(titleCase("AdItIa DaRmADi jIWaNdoNO"));
+console.log(titleCase("AdItIa DaRmADi jIWaNdoNO"))
